7ef4e933e1bf7db14eba8fe20b7dd978e6752670
Lukas Klausner <KlausnerL22066@sus.htl-grieskirchen.at>
2024-09-24 15:13:40 +0200

    Implemented the primeTester Object

--- Lambda/src/beispiel3/htlgkr/KlausnerL22066/NumberTester.java
+++ Lambda/src/beispiel3/htlgkr/KlausnerL22066/NumberTester.java
@@ -3,6 +3,17 @@ package beispiel3.htlgkr.KlausnerL22066;
 public class NumberTester {
 
     NumberTest oddTester = (number) -> number % 2 != 0;
+    NumberTest primeTester = (number) -> {
+        if (number <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= number; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    };
 
     public NumberTester(String filename){
 
@@ -10,5 +21,9 @@ public class NumberTester {
 
     public void setOddEvenTester(NumberTest oddTester){
         this.oddTester = oddTester;
+    }
+
+    public void setPrimeTester(NumberTest primeTester){
+        this.primeTester = primeTester;
     }
 }
