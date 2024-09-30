7ce29ce06d79fa89266d6bc95e8fe5579614d8c5
Lukas Klausner <KlausnerL22066@sus.htl-grieskirchen.at>
2024-09-30 17:37:52 +0200

    implemented all methods from the class RationalCalculator

--- Lambda/src/beispiel4/htlgkr/KlausnerL22066/RationalCalculator.java
+++ Lambda/src/beispiel4/htlgkr/KlausnerL22066/RationalCalculator.java
@@ -1,4 +1,34 @@
 package beispiel4.htlgkr.KlausnerL22066;
 
-public class RationalCalculator {
+public class RationalCalculator extends AbstractCalculator{
+
+
+    public RationalCalculator(CalculationOperation add, CalculationOperation subtract, CalculationOperation multiply, CalculationOperation divide) {
+        super(
+                (x, y) -> new Number(x.getA() + y.getA(), 0),
+                (x, y) -> new Number(x.getA() - y.getA(), 0),
+                (x, y) -> new Number(x.getA() * y.getA(), 0),
+                (x, y) -> new Number(x.getA() / y.getA(), 0)
+        );
+    }
+
+    @Override
+    public Number add(Number x, Number y) {
+        return add.calc(x, y);
+    }
+
+    @Override
+    public Number subtract(Number x, Number y) {
+        return subtract.calc(x, y);
+    }
+
+    @Override
+    public Number multiply(Number x, Number y) {
+        return multiply.calc(x, y);
+    }
+
+    @Override
+    public Number divide(Number x, Number y) {
+        return divide.calc(x, y);
+    }
 }
